4643d4ac04f2bc6a1ffb9c0ba3e819bcbf4fd9ee
Nidhi Sadhu <nidhi.sadhu2097@gmail.com>
2026-08-25 16:08:06 -0400

    feat: header navigation and account menu

---
+++ ui/src/app/modules/cart/feature/cart-page/cart-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cart-page',
+  styleUrl: './cart-page.scss',
+  templateUrl: './cart-page.html',
+})
+export class CartPage {}
