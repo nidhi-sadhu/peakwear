8858819a60b4b284e165887ad1e6aa1e670e5501
Nidhi Sadhu <nidhi.sadhu2097@gmail.com>
2026-08-25 15:32:52 -0400

    feat: Angular Material styling for auth pages and header

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginStore } from '@modules/login/data-access/login.store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  readonly store = inject(LoginStore);
+
+  constructor() {
+    this.store.hydrate();
+  }
+}
